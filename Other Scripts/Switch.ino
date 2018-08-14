5ce6ca1ca5a97c3646215732307072c8e6f1bbc6
phenotypic <33377034+phenotypic@users.noreply.github.com>
2018-08-15 00:59:22 +0200

    Add files via upload

---
+++ Other Scripts/Switch.ino
@@ -0,0 +1,122 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+
+// GitHub Page = https://github.com/Tommrodrigues/homebridge-nodemcu-relay
+
+// Script Type = Relay Switch
+
+// D4 = LOW activation
+// D7 = HIGH activation
+
+/////////////////// CHANGE THESE VALUES //////////////////////
+const char* ssid = "SSID"; //Name of your network
+const char* password = "PASSWORD"; //Password for your network
+//////////////////////////////////////////////////////////////
+
+const int highPin = 13; //Declares "highPin" being pin 13 (D7) on NodeMCU
+const int lowPin = 2; //Declaers "lowPin" being pin 2 (D4) on NodeMCU
+const int redPin = 16; //Declaers "redPin" being pin 16 (Red LED)
+WiFiServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  pinMode(lowPin, OUTPUT);
+  pinMode(highPin, OUTPUT);
+  pinMode(redPin, OUTPUT);
+  digitalWrite(highPin, LOW);
+  digitalWrite(lowPin, HIGH);
+  digitalWrite(redPin, LOW);
+
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+
+  // Print the IP address
+  Serial.print("Use this URL to connect: ");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+
+  // Start the mDNS responder for nodemcu.local
+  if (!MDNS.begin("nodemcu")) {
+    Serial.println("Error setting up MDNS responder!");
+  }
+  Serial.println("mDNS responder started");
+
+  digitalWrite(redPin, HIGH);
+
+}
+
+void loop() {
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+
+  // Wait until the client sends some data
+  Serial.println("New client");
+  while(!client.available()){
+    delay(1);
+  }
+
+  // Read the first line of the request
+  String request = client.readStringUntil('\r');
+  Serial.println(request);
+  client.flush();
+
+  // Match the request
+
+  int value = LOW;
+  if (request.indexOf("/SWITCH=ON") != -1)  {
+    digitalWrite(highPin, HIGH);
+    digitalWrite(lowPin, LOW);
+    value = HIGH;
+  }
+  if (request.indexOf("/SWITCH=OFF") != -1)  {
+    digitalWrite(highPin, LOW);
+    digitalWrite(lowPin, HIGH);
+    value = LOW;
+  }
+
+  // Return the response
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println("");
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+
+  client.print("Current state: ");
+
+  if(value == HIGH) {
+    client.print("On");
+  } else {
+    client.print("Off");
+  }
+  client.println("<br><br>");
+  client.println("<a href=\"/SWITCH=ON\"\"><button>Turn On </button></a>");
+  client.println("<a href=\"/SWITCH=OFF\"\"><button>Turn Off </button></a><br />");
+  client.println("</html>");
+
+  delay(1);
+  Serial.println("Client disonnected");
+  Serial.println("");
+
+}
